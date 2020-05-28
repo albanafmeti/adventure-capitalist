4d233bf9abaabcd7bcc438c40fccf5316424f887
Alban Afmeti <albanafmeti@gmail.com>
2020-05-28 23:22:31 +0200

    updated

--- backend/src/repositories/Repository.js
+++ backend/src/repositories/Repository.js
@@ -1,18 +1,7 @@
-const HttpClient = require('../services/HttpClient');
-
 /**
  * Parent repository class.
  */
 class Repository {
-
-    /**
-     * Set bearer token to the http client.
-     * @param token
-     */
-    static setAccessToken(token) {
-        HttpClient.bearerToken(token);
-        return Repository;
-    }
 }
 
 module.exports = Repository;
