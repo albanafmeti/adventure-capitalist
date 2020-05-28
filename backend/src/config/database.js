7087cf15e4fc6abf7d2a0c3df7c5aede3ff76588
Alban Afmeti <albanafmeti@gmail.com>
2020-05-28 19:20:02 +0200

    updated

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const HOST = process.env.DATABASE_HOST;
+const PORT = process.env.DATABASE_PORT;
 const USER = process.env.DATABASE_USER;
 const PASSWORD = process.env.DATABASE_PASSWORD;
 const DATABASE = process.env.DATABASE_NAME;
@@ -11,25 +12,19 @@ module.exports = {
         password: PASSWORD,
         database: DATABASE,
         host: HOST,
+        port: PORT,
         dialect: "mysql",
         migrationStorageTableName: "migrations",
         dialectOptions: {
             multipleStatements: true
         }
     },
-    test: {
-        username: USER,
-        password: PASSWORD,
-        database: DATABASE,
-        host: 'localhost',
-        dialect: "mysql",
-        migrationStorageTableName: "migrations"
-    },
     production: {
         username: USER,
         password: PASSWORD,
         database: DATABASE,
         host: HOST,
+        port: PORT,
         dialect: "mysql",
         migrationStorageTableName: "migrations"
     }
--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,12 +1,14 @@
 const Sequelize = require('sequelize');
 
 const HOST = process.env.DATABASE_HOST;
+const PORT = process.env.DATABASE_PORT;
 const USER = process.env.DATABASE_USER;
 const PASSWORD = process.env.DATABASE_PASSWORD;
 const DATABASE = process.env.DATABASE_NAME;
 
 const sequelize = new Sequelize(DATABASE, USER, PASSWORD, {
     host: HOST,
+    port: PORT,
     dialect: 'mysql',
     pool: {
         max: 5,
