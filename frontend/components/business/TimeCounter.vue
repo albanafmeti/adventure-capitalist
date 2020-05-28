0c473381974e4201977e776cb0987011337b5c0f
Alban Afmeti <albanafmeti@gmail.com>
2020-05-29 00:54:33 +0200

    updated

--- frontend/components/business/TimeCounter.vue
+++ frontend/components/business/TimeCounter.vue
@@ -36,10 +36,10 @@
                 this.timeIntervals.push(interval);
             },
             toHHMMSS(secs) {
-                var sec_num = parseInt(secs, 10)
-                var hours = Math.floor(sec_num / 3600)
-                var minutes = Math.floor(sec_num / 60) % 60
-                var seconds = sec_num % 60
+                var sec_num = parseInt(secs, 10);
+                var hours = Math.floor(sec_num / 3600);
+                var minutes = Math.floor(sec_num / 60) % 60;
+                var seconds = sec_num % 60;
 
                 return [hours, minutes, seconds]
                     .map(v => v < 10 ? "0" + v : v)
